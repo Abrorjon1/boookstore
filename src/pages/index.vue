9400e8a93150856cc53ee6974697edbb64f2dded
Mirjalol Salimov official page <79058363+offism@users.noreply.github.com>
2022-12-02 16:58:08 +0300

    2.12-c1

---
+++ src/pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <NavbarSection />
+    <FeaturedSection />
+    <CategoriesSection />
+    <OfferSection />
+    <ProductsSection />
+    <SubscribeSection />
+  </div>
+</template>
+
+<script>
+import NavbarSection from "../components/NavbarSection.vue";
+import FeaturedSection from "../components/FeaturedSection.vue";
+import CategoriesSection from "../components/CategoriesSection.vue";
+import OfferSection from "../components/OfferSection.vue";
+import ProductsSection from "../components/ProductsSection.vue";
+import SubscribeSection from "../components/SubscribeSection.vue";
+
+export default {
+  name: "Home",
+  components: {
+    FeaturedSection,
+    NavbarSection,
+    OfferSection,
+    CategoriesSection,
+    SubscribeSection,
+    ProductsSection,
+  },
+};
+</script>
+
+<style></style>
