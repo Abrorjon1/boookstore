1e4a4c13169df8226797a40718a6ec9eeeddb2fb
Mirjalol Salimov official page <79058363+offism@users.noreply.github.com>
2022-12-03 14:05:14 +0300

    3.12-c1

--- src/components/VendorSection.vue
+++ src/components/VendorSection.vue
@@ -1,39 +1,16 @@
 <template>
-  <div>
-    <!-- Vendor Start -->
-    <div class="container-fluid py-5">
-      <div class="row px-xl-5">
-        <div class="col">
-          <div class="owl-carousel vendor-carousel">
-            <div class="vendor-item border p-4">
-              <img src="img/vendor-1.jpg" alt="" />
-            </div>
-            <div class="vendor-item border p-4">
-              <img src="img/vendor-2.jpg" alt="" />
-            </div>
-            <div class="vendor-item border p-4">
-              <img src="img/vendor-3.jpg" alt="" />
-            </div>
-            <div class="vendor-item border p-4">
-              <img src="img/vendor-4.jpg" alt="" />
-            </div>
-            <div class="vendor-item border p-4">
-              <img src="img/vendor-5.jpg" alt="" />
-            </div>
-            <div class="vendor-item border p-4">
-              <img src="img/vendor-6.jpg" alt="" />
-            </div>
-            <div class="vendor-item border p-4">
-              <img src="img/vendor-7.jpg" alt="" />
-            </div>
-            <div class="vendor-item border p-4">
-              <img src="img/vendor-8.jpg" alt="" />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- Vendor End -->
+  <div
+    class="gallery js-flickity"
+    data-flickity-options='{ "wrapAround": true }'
+  >
+    <div class="gallery-cell one"></div>
+    <div class="gallery-cell two"></div>
+    <div class="gallery-cell three"></div>
+    <div class="gallery-cell four"></div>
+    <div class="gallery-cell five"></div>
+    <div class="gallery-cell six"></div>
+    <div class="gallery-cell seven"></div>
+    <div class="gallery-cell eight"></div>
   </div>
 </template>
 
@@ -42,5 +19,47 @@ export default {
   name: "VendorSection",
 };
 </script>
+<style scoped>
+.gallery {
+  background: white;
+}
 
-<style></style>
+.gallery-cell {
+  width: 33%;
+  height: 200px;
+  margin-right: 10px;
+  counter-increment: gallery-cell;
+}
+.gallery-cell.one {
+  background: url("./../../public/img/vendor-1.jpg") center center no-repeat;
+}
+.gallery-cell.two {
+  background: url("./../../public/img/vendor-2.jpg") center center no-repeat;
+}
+.gallery-cell.three {
+  background: url("./../../public/img/vendor-3.jpg") center center no-repeat;
+}
+.gallery-cell.four {
+  background: url("./../../public/img/vendor-4.jpg") center center no-repeat;
+}
+.gallery-cell.five {
+  background: url("./../../public/img/vendor-5.jpg") center center no-repeat;
+}
+.gallery-cell.six {
+  background: url("./../../public/img/vendor-6.jpg") center center no-repeat;
+}
+.gallery-cell.seven {
+  background: url("./../../public/img/vendor-7.jpg") center center no-repeat;
+}
+.gallery-cell.eight {
+  background: url("./../../public/img/vendor-8.jpg") center center no-repeat;
+}
+.gallery-cell:before {
+  display: block;
+  text-align: center;
+  content: counter(gallery-cell);
+  line-height: 200px;
+  font-size: 80px;
+  color: white;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import("../public/lib/owlcarousel/assets/owl.carousel.min.css");
 import("./assets/style.css");
 Vue.config.productionTip = false;
 
